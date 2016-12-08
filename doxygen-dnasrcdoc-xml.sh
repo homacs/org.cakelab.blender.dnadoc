ef193d4298d493e1b17baf419c7df7fd6f67bbd5
homac <homac@strace.org>
2016-12-08 11:17:52 +0100

    see previous commit -.-

--- doxygen-dnasrcdoc-xml.sh
+++ doxygen-dnasrcdoc-xml.sh
@@ -11,17 +11,113 @@
 
 
 
+################## CONFIG SECTION ##################
+
+# BLENDER_VERSION
+#
+# Major and minor part of blender version string
+BLENDER_VERSION=2.78
+
+# ENV_PATH_BASE
+#
+# Path to blender source code directory
+ENV_PATH_BASE="/home/homac/workspaces/blender/blender-2.78a"
+
+# REL_SRC_PATH
+#
+# Relative path to DNA structs (shouldn't change soon)
+REL_SRC_PATH="source/blender/makesdna"
+
+# OUTPUT_PATH
+# 
+# This is the root path of the target Java.Blend documentation path.
+OUTPUT_PATH="/home/homac/repos/git/github/homacs/JavaBlendDocs/resources/dnadoc"
+
+# ALL_COMMENTS
+#
+# Whether to convert existing documentation into doxygen
+# conform documentation.
+ALL_COMMENTS=true
+
+# JDOXML_CLASSPATH
+JDOXML_CLASSPATH="/home/homac/repos/svn/cakelab.org/playground/org.cakelab.jdoxml/bin"
+
+# JSON_CLASSPATH
+JSON_CLASSPATH="/home/homac/repos/svn/cakelab.org/playground/org.cakelab.json/bin"
+
+# DNADOC_CLASSPATH
+JAVA_BLEND_CLASSPATH="/home/homac/repos/svn/cakelab.org/playground/org.cakelab.blender.io/bin"
 
 
-BLENDER_VERSION=2.69
-
-export ENV_PATH_BASE="/home/homac/workspace-sb6jcode"
-export ENV_INPUT="$ENV_PATH_BASE/blender-$BLENDER_VERSION/source/blender/makesdna"
-export ENV_OUTPUT_DIR="/tmp/blender-$BLENDER_VERSION-xmldoc"
+################## CONFIG SECTION END ###############
 
 
-doxygen ./blender-${BLENDER_VERSION}-dnasrc-xml.doxygen
-pushd $ENV_OUTPUT_DIR
-xsltproc combine.xslt index.xml >/tmp/blender-$BLENDER_VERSION-all.xml
-popd
+ENV_INPUT="$ENV_PATH_BASE/$REL_SRC_PATH"
+ENV_OUTPUT_DIR="/tmp/blender-$BLENDER_VERSION-xmldoc"
 
+
+#
+# This function preprocesses *.c *.cpp *.h *.hpp
+# and replaces the following:
+# - "/*" -> "/**"
+#
+function preprocess () {
+	dir="$1"
+	ftmp=`mktemp /tmp/preprocessing.XXXXXX`
+	# enumerate all C source code files
+	find $dir -regex ".*\.\(c\|h\)\(pp\)?$" | while read f ; do
+		echo "$f ..."
+		cat $f | awk '{ \
+			s = $0 ; \
+			s = gensub("[[:space:]]*DNA_DEPRECATED[[:space:]]*([,;][[:space:]]*)/\\*([^\\*!])", "\\1/**< \\\\deprecated\\2", "g", s); \
+			s = gensub("(;[[:space:]]*)/\\*([^\\*!])", "\\1/**<\\2", "g", s); \
+			s = gensub("[[:space:]]*DNA_DEPRECATED[[:space:]]*([,;][[:space:]]*)//([^/!])", "\\1///< \\\\deprecated\\2", "g", s); \
+			s = gensub("(;[[:space:]]*)//([^/!])", "\\1///<\\2", "g", s); \
+			s = gensub("//([^/!])", "///\\1", "g", s); \
+			s = gensub("/\\*([^\\*!])", "/**\\1", "g", s); \
+			s = gensub("/\\*$", "/**", "g", s); \
+			s = gensub("[[:space:]]*DNA_DEPRECATED[[:space:]]*([,;][[:space:]]*)", "\\1/**< \\\\deprecated */ \\2", "G", s); \
+			print s \
+		}'	 > $ftmp
+		mv $ftmp $f
+	done
+}	
+
+
+
+
+if $ALL_COMMENTS ; then
+	# preprocess the source files and change every non-doxygen comment into a
+	# doxygen conform comment.
+
+	# make a clean copy
+	tmp="/tmp/blender-$BLENDER_VERSION-preprocessed"
+	rm -rf $tmp 
+	mkdir -p $tmp/$REL_SRC_PATH/.
+	cp -r $ENV_INPUT/* $tmp/$REL_SRC_PATH/.
+
+	# run preprocessing over copy
+	preprocess $tmp
+	
+	# update environment variables
+	export ENV_PATH_BASE="$tmp"
+	export ENV_INPUT="$ENV_PATH_BASE/$REL_SRC_PATH"
+	export ENV_OUTPUT_DIR="/tmp/blender-$BLENDER_VERSION-xmldoc"
+fi
+
+
+# export environment variables to pass them to doxygen 
+# (they are used in the doxygen config)
+export ENV_PATH_BASE
+export ENV_INPUT
+export ENV_OUTPUT_DIR
+
+# run doxygen
+# config is in ./doxygen-dnasrcdoc-xml.doxygen
+# result goes in $ENV_OUTPUT_DIR
+rm -rf $ENV_OUTPUT_DIR
+doxygen ./blender-dnasrc-xml.doxygen
+
+# extract documentation to dnadoc
+CLASSPATH="$JDOXML_CLASSPATH:$JSON_CLASSPATH:$JAVA_BLEND_CLASSPATH"
+java -cp $CLASSPATH org.cakelab.blender.doc.extract.dnadocs.Main -in "$ENV_OUTPUT_DIR" -out "$OUTPUT_PATH" -v "$BLENDER_VERSION"
--- extract-pyapi-docs.sh
+++ extract-pyapi-docs.sh
@@ -3,7 +3,7 @@
 #
 # This is a script which calls blender with a blender standard
 # python script to receive all docs from blender python API.
-# Those are related to on RNA, but RNA is kind of derived from
+# Those are related to RNA, but RNA is kind of derived from
 # DNA.
 #
 # The output of the script is fed to the class:
@@ -13,8 +13,48 @@
 # -homac
 #
 
+# Some default variables (maybe overridden by the user in config section below)
+# using standard installation.
+BLENDER_SYSTEM_SCRIPTS="/usr/share/blender/scripts"
+BLENDER=~/opt/blender-2.78a-linux-glibc211-x86_64/blender
+
+
+
 
 ################# CONFIGURATION SECTION ################
+
+# BLENDER_BUILD_ENV
+#
+# This variable controls whether the script uses the standard
+# installation or a custom built installation in a different
+# directory.
+BLENDER_BUILD_ENV=false
+
+# BLENDER_SOURCE
+#
+# Required only if BLENDER_BUILD_ENV is set to true.
+# Path to blender source code. 
+BLENDER_SOURCE="/home/homac/repos/git/blender.org/blender"
+
+# BLENDER_BUILD
+#
+# Required only if BLENDER_BUILD_ENV is set to true.
+# Specifies the blender build directory.
+# This directory has to contain the following directories
+# - bin: containing the blender executable
+# - lib: containing all custom build third-party libraries
+BLENDER_BUILD="$BLENDER_SOURCE/build"
+
+
+# JSON_CLASSPATH
+# 
+# classpath for dependency org.cakelab.json
+JSON_CLASSPATH="/home/homac/repos/svn/cakelab.org/playground/org.cakelab.json/bin"
+
+# JAVA_BLEND_CLASSPATH
+# 
+# Java .Blend SDK class path
+JAVA_BLEND_CLASSPATH="/home/homac/repos/svn/cakelab.org/playground/org.cakelab.blender.io/bin"
 
 #
 # SCRIPT
@@ -24,7 +64,7 @@
 # there then use the following command to locate it:
 # > updatedb && locate rna_info.py
 #
-SCRIPT=/usr/share/blender/scripts/modules/rna_info.py
+SCRIPT="$BLENDER_SYSTEM_SCRIPTS/modules/rna_info.py"
 
 #
 # OUTPUT
@@ -37,13 +77,17 @@ OUTPUT="resources/dnadoc"
 
 #
 # VERSION
-# This is the version of the Blender installed on your system.
+#
+# Default: <empty>
+#
+# This is the version of Blender executable used to receive 
+# python api documentation. If left empty, the script will determine
+# the version on its own using `blender -v`.
+#
 # This version information is used to create a subfolder in the
 # documentation system. 
-# We receive the version from the Blender process, but you can 
-# even set a fixed version number.
 #
-VERSION=`blender -v | grep "Blender" | head -n 1 | awk '{print $2}'`
+VERSION=
 
 #
 # TMP
@@ -54,10 +98,28 @@ TMP=/tmp
 
 ################# END OF CONFIGURATION SECTION ################
 
+if $BLENDER_BUILD_ENV ; then
+	# preparing to run the custom build blender executable
 
+	export BLENDER_SYSTEM_DATAFILES="$BLENDER_SOURCE/release/datafiles"
+	export BLENDER_SYSTEM_SCRIPTS="$BLENDER_SOURCE/release/scripts"
+	export LD_LIBRARY_PATH="$BLENDER_BUILD/lib"
+
+	# BLENDER
+	#
+	# The blender executable.
+	BLENDER="$BLENDER_BUILD/bin/blender"
+fi
+
+
+if [ -z "$VERSION" ] ; then
+	echo "retrieving blender version string"
+	VERSION=`$BLENDER -v | grep "Blender" | head -n 1 | awk '{print $2}'`
+fi
 
 
 echo "extracting blenders python api documentation."
+echo "blender executable: ${BLENDER}"
 echo "blender version: ${VERSION}"
 echo "running python script: ${SCRIPT}"
 
@@ -67,7 +129,7 @@ SCRIPT_OUTPUT=$TMP/pyapi-${VERSION}.txt
 CONVERTER_INPUT=$TMP/pyapi-${VERSION}-clean.txt
 
 rm -f $SCRIPT_OUTPUT
-blender --background -noaudio --python $SCRIPT 2> $SCRIPT_OUTPUT >/dev/null
+$BLENDER --background -noaudio --python $SCRIPT 2> $SCRIPT_OUTPUT >/dev/null
 
 echo "removing debug output"
 cat $SCRIPT_OUTPUT | while read line && [ "$line" != "EOF" ] ; do 
@@ -84,12 +146,13 @@ done > $CONVERTER_INPUT
 
 echo "calling converter class: org.cakelab.blender.doc.extract.ExtractPyAPIDoc"
 
-CLASSPATH=./bin:../org.cakelab.json/bin
-CLASSPATH=${CLASSPATH}`find lib -name "*.jar" | while read jar ; do 
-	echo -n ":$jar"
-done`
+CLASSPATH="$JSON_CLASSPATH:$JAVA_BLEND_CLASSPATH"
+#CLASSPATH=${CLASSPATH}`find lib -name "*.jar" | while read jar ; do 
+	#	echo -n ":$jar"
+#done`
+echo "CLASSPATH=$CLASSPATH"
+java -cp ${CLASSPATH} org.cakelab.blender.doc.extract.rnadocs.ExtractPyAPIDoc -v ${VERSION} -in $CONVERTER_INPUT -out ${OUTPUT}
 
-java -cp ${CLASSPATH} org.cakelab.blender.doc.extract.ExtractPyAPIDoc -v ${VERSION} -in $CONVERTER_INPUT -out ${OUTPUT}
 
 rm -f $SCRIPT_OUTPUT
 rm -f $CONVERTER_INPUT
